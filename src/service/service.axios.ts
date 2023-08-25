3f779dacc0138c6102def9a9434978671f2e8437
Samishan11 <samishanthapa0@gmail.com>
2023-08-25 19:33:29 +0545

    changes

--- src/service/service.axios.ts
+++ src/service/service.axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { token } from "./token";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URI,
   headers: {
     authorization: `Bearer ${token}`,
     "Content-type": "application/json",
